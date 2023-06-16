6140b3b01cbda79201e20d11073bb731484b40dc
Ktamim98 <tamim15-2515@diu.edu.bd>
2023-06-16 16:55:47 +0600

    Initial Commit

---
+++ UberClone/App/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Tamim Khan on 16/6/23.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ UberClone/Manager/LocationManager.swift
@@ -0,0 +1,33 @@
+//
+//  LocationManager.swift
+//  UberClone
+//
+//  Created by Tamim Khan on 16/6/23.
+//
+
+import CoreLocation
+
+
+class LocationManager: NSObject, ObservableObject{
+    
+    private let locationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    
+    
+    
+}
+
+extension LocationManager: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard !locations.isEmpty else {return}
+        locationManager.stopUpdatingLocation()
+    }
+}
